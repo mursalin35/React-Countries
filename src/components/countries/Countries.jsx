3e3c1c726a79d3df33a0e6f1bfe7cef62535bf16
mursalin35 <msmursalin35@gmail.com>
2025-09-19 21:44:44 +0600

    new countries

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>In the Countries word</h1>
+        </div>
+    );
+};
+
+export default Countries;
